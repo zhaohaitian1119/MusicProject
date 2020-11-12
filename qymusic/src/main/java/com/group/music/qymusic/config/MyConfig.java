3ae434922b9bf4683eef4bc307ffa9d04c1370f9
zhaohaitian <397408662@qq.com>
2020-11-12 11:02:31 +0800

    项目基础搭建

--- qymusic/src/main/java/com/group/music/qymusic/config/MyConfig.java
+++ qymusic/src/main/java/com/group/music/qymusic/config/MyConfig.java
@@ -14,5 +14,17 @@ public class MyConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index.html");
+        registry.addViewController("/bangdan.html").setViewName("bangdan.html");
+        registry.addViewController("/musician.html").setViewName("musician.html");
+        registry.addViewController("/musicRadio.html").setViewName("musicRadio.html");
+        registry.addViewController("/musicSearchTest.html").setViewName("musicSearchTest.html");
+        registry.addViewController("/mv.html").setViewName("mv.html");
+        registry.addViewController("/mymusic.html").setViewName("mymusic.html");
+        registry.addViewController("/QQmusic.html").setViewName("QQmusic.html");
+        registry.addViewController("/register.html").setViewName("register.html");
+        registry.addViewController("/resetpassword.html").setViewName("resetpassword.html");
+        registry.addViewController("/singer.html").setViewName("singer.html");
+        registry.addViewController("/songlist.html").setViewName("songlist.html");
+        registry.addViewController("/test.html").setViewName("test.html");
     }
 }
